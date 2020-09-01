554418a4c80eac1cc9632dac17e40a44e9f29c1c
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-01 17:34:47 -0500

    Implemented a polar rose animation

--- master.js
+++ master.js
@@ -1,3 +1,8 @@
+var k = 0,
+    PI = Math.PI,
+    mag = 300,
+    delta = 0.01;
+
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
@@ -18,12 +23,31 @@ function init() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
   ctx.closePath();
+
+  ctx.translate(WIDTH/2, HEIGHT/2);
+  ctx.strokeStyle = "white";
   
   ani();
 }
 
 function ani() {
+    ctx.fillRect(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT);
+    ctx.beginPath();
+    ctx.moveTo(300, 0);
+    for (var i = 0; i<= PI*2; i+= delta) {
+      var r = Math.cos(k * i) * mag;
   
+      var pos = Vector.fromAngle(i).mult(r);
+      ctx.lineTo(pos.x, pos.y);
   
-  //requestAnimationFrame(ani);
+      // point(pos.x, pos.y, 'white', 1);
+    }
+    ctx.stroke();
+    ctx.closePath();
+
+    k += 0.005;
+  
+  requestAnimationFrame(ani);
 }
+
+init();
